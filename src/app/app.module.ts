ed1259bb4ca06864fc8f7b604567d1e36326eda9
MartinMinuto <martin.minuto1@gmail.com>
2023-04-17 18:56:13 -0300

    1.6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
+import { TeamComponent } from './team/team.component';
+import { TopComponent } from './top/top.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerComponent
+    PlayerComponent,
+    TeamComponent,
+    TopComponent
   ],
   imports: [
     BrowserModule,
